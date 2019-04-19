5855bd9b8e1c948005b4569e99974199577078fa
Steve Richardson <tangentaudio@gmail.com>
2019-04-18 21:49:35 -0400

    new doorbot wiegand code updates

--- examples/WiegandTest/WiegandTest.ino
+++ examples/WiegandTest/WiegandTest.ino
@@ -1,29 +1,30 @@
-#include <Wiegand.h>
+  #include <Wiegand.h>
 
 WIEGAND wg;
 
+#define LED_DOORBOT 8
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_DOORBOT, OUTPUT);
 	Serial.begin(9600);  
 	wg.begin();
-    Serial.print("HI");
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH); 
+  digitalWrite(LED_DOORBOT, HIGH); 
   delay(200);
 
 	if (wg.available()) {
    Serial.write(2); 
-		Serial.print(wg.getCode(),HEX);
-      Serial.write(3); 
+	 Serial.print(wg.getCode(), HEX);
+   Serial.write(3); 
 
-	//	Serial.print(wg.getCode());
-//		Serial.print(", Type W");
-	//	Serial.println(wg.getWiegandType());  
-      delay(100);   
+//	Serial.print(wg.getCode());
+//	Serial.print(", Type W");
+//	Serial.println(wg.getWiegandType());  
+    delay(100);   
 	}
-  digitalWrite(LED_BUILTIN, LOW); 
+  digitalWrite(LED_DOORBOT, LOW); 
   delay(150); 
   
 }
