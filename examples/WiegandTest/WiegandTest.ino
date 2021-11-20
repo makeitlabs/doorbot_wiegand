42bb6a747aa6b6a4b895fa5a39283a993678b844
tangentaudio <tangentaudio@users.noreply.github.com>
2021-11-20 11:13:38 -0500

    cleanup

--- examples/WiegandTest/WiegandTest.ino
+++ examples/WiegandTest/WiegandTest.ino
@@ -12,19 +12,14 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_DOORBOT, HIGH); 
-  delay(200);
-
+  delay(100);
 	if (wg.available()) {
-   Serial.write(2); 
-	 Serial.print(wg.getCode(), HEX);
-   Serial.write(3); 
-
-//	Serial.print(wg.getCode());
-//	Serial.print(", Type W");
-//	Serial.println(wg.getWiegandType());  
+    Serial.write(2); 
+	  Serial.print(wg.getCode(), HEX);
+    Serial.write(3); 
     delay(100);   
 	}
   digitalWrite(LED_DOORBOT, LOW); 
-  delay(150); 
+  delay(100); 
   
 }
